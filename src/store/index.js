1a12713f86138a6e5ecda24c18c99bd1a12da2c9
jraynolds <jaspirian@gmail.com>
2020-11-14 00:21:33 -0800

    VueX used

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,72 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from 'firebase/app'
 
 Vue.use(Vuex)
 
+const initialState = () => {
+	return {
+		user: null,
+		activeCharacter: 0,
+		error: null
+	}
+}
+
 export default new Vuex.Store({
-  state: {
-  },
+	state: initialState(),
+	getters: {
+		getUser(state) {
+			return state.user;
+		},
+		isUserAuth(state) {
+			return !!state.user;
+		},
+		getError(state) {
+			return state.error;
+		}
+	},
   mutations: {
+		setUser(state, payload) {
+			state.user = payload;
+		},
+		setError(state, payload) {
+			state.error = payload;
+		}
   },
   actions: {
+		signUp({ commit }, payload) {
+			firebase
+				.auth()
+				.createUserWithEmailAndPassword(payload.email, payload.password)
+				.then(response => {
+					commit("setUser", response.user);
+				})
+				.catch(error => {
+					commit("setError", error.message);
+				});
+		},
+		signIn({ commit }, payload) {
+			return firebase
+				.auth()
+				.signInWithEmailAndPassword(payload.email, payload.password)
+				.then(response => {
+					commit("setUser", response.user);
+				})
+				.catch(error => {
+					commit("setError", error.message);
+				});
+		},
+		signOut({ commit }, payload) {
+			firebase
+				.auth()
+				.signOut()
+				.then(() => {
+					commit("setUser", null);
+				})
+				.catch(error => {
+					commit("setError", error.message);
+				});
+		}
   },
   modules: {
   }
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+	],
+	// module: {
+	// 	rules: [
+	// 		{ text: /\.ejs$/, loader: 'ejs-loader' }
+	// 	]
+	// },
+	// plugins: [
+	// 	new HtmlWebpackPlugin({
+	// 		template: 'src/template.ejs'
+	// 	})
+	// ]
 }
